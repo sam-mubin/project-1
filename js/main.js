634cbac304982db5bd5b6cdd2e619ba030271e67
sam-mubin <sheikhabdulmubin@gmail.com>
2023-08-28 18:17:36 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//clos btn
+
+function openMenu() {
+    document.getElementById("menu-container").style.width="100%"
+}
+
+function closMenu() {
+    document.getElementById("menu-container").style.width="0"
+}
+
+
+// MIXIITUP
+
+var mixer = mixitup('.work-grid');
